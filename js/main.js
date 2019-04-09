037ebddbf1d67ca52055dc123a6e658c77357f47
valeryho <v.gorodskov1985@gmail.com>
2019-04-09 19:40:33 +0300

    commit 5-api added

--- js/main.js
+++ js/main.js
@@ -4,8 +4,17 @@ window.addEventListener("load",Init);
 function Init()
 {
     console.log("init");
-    var url = "https://newsapi.org/v2/top-headlines?country=ua&apiKey=9a74d04fa22a42b6bfe20a4df5895f97";
-    Request(url,SportNews);
+    var url_sport = "https://newsapi.org/v2/top-headlines?country=ua&category=sports&apiKey=9a74d04fa22a42b6bfe20a4df5895f97";
+    Request(url_sport,SportNews);
+    var url_Enterteiment = "https://newsapi.org/v2/top-headlines?country=ua&category=entertainment&apiKey=9a74d04fa22a42b6bfe20a4df5895f97";
+    Request(url_Enterteiment, EnterteimentNews);
+    var url_Helth = "https://newsapi.org/v2/top-headlines?country=ua&category=health&apiKey=9a74d04fa22a42b6bfe20a4df5895f97";
+    Request(url_Helth, HelthNews);
+    var url_Technology = "https://newsapi.org/v2/top-headlines?country=ua&category=technology&apiKey=9a74d04fa22a42b6bfe20a4df5895f97";
+    Request(url_Technology, TechnologyNews);
+    var url_Science = "https://newsapi.org/v2/top-headlines?country=ua&category=technology&apiKey=9a74d04fa22a42b6bfe20a4df5895f97";
+    Request(url_Science, ScienceNews);
+
 }
 
 function Request(url,calback)
@@ -32,5 +41,17 @@ function Request(url,calback)
 }
 function SportNews(data)
 {
+    console.log(data);
+}
+function EnterteimentNews(data) {
+    console.log(data);
+}
+function HelthNews(data) {
+    console.log(data);
+}
+function TechnologyNews(data) {
+    console.log(data);
+}
+function ScienceNews(data) {
     console.log(data);
 }
